ed18c129ad0d682506925d9a30ce3edfb4e4f21b
Eshini Rathnayake <126976004+eshinirathnayake@users.noreply.github.com>
2026-03-17 23:26:45 +0530

    file upload screen added.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'screens/onboarding_screen1.dart';
 import 'screens/onboarding_screen4.dart';
 import 'screens/onboarding_screen2.dart';
 import 'screens/onboarding_screen3.dart';
-
+import 'screens/file_upload_screen.dart';
 //import 'screens/learning_path_screen.dart';
 //import 'screens/quiz_screen.dart';
 //import 'screens/progress_screen.dart';
@@ -71,6 +71,7 @@ class StudySparkApp extends StatelessWidget {
         '/onboard4': (context) => const OnboardingScreen4(),
         '/questionnaire': (context) =>
             const PersonalityTestQuestionnaireScreen(),
+        '/upload': (context) => const FileUploadScreen(),
 
         //'/learning-path': (context) => const LearningPathScreen(),
         //'/quiz': (context) => const QuizScreen(),
