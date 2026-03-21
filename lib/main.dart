7a527a4baa46340c2f92dc3c99e3c4c317f4fb67
Sasindu Sanka <sasindusanka@gmail.com>
2026-03-21 17:59:22 +0530

    file upload integration

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'screens/onboarding_screen1.dart';
 import 'screens/onboarding_screen4.dart';
 import 'screens/onboarding_screen2.dart';
 import 'screens/onboarding_screen3.dart';
-import 'screens/library_screen.dart';
+import 'screens/Library_screen.dart';
 import 'screens/practice_screen.dart';
 import 'screens/outputresult_screen.dart';
 import 'package:studyspark/env.dart';
